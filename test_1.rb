a6c2c9a4a161dd24903e312e00d2199bb8ccac5b
sujan kumar reddy challa <sujankumar0510@gmail.com>
2016-12-29 08:37:30 -0800

    Update test_1.rb

--- test_1.rb
+++ test_1.rb
@@ -36,14 +36,15 @@ end
 class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-
-    if @user.save
-      ApplicationMailer.welcome(@user).deliver
-      format.html { redirect_to @user, notice: 'User was successfully created.' }
-      format.json { render :show, status: :created, location: @user }
-    else
-      format.html { render :new }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @user.save
+        ApplicationMailer.welcome(@user).deliver
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
